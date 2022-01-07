458fc6fc6f41aeb7ed32201afef0dc90224e6ef4
Lennin <80495420+LenninZiony@users.noreply.github.com>
2022-01-06 22:23:14 -0300

    :)

---
+++ installer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo '   __  ___         _      ___                  __             __'
+echo '  /  |/  /__ _____(_)__ _/ _ \___ _    _____  / /__  ___ ____/ /__ ____'
+echo ' / /|_/ / _ `/ __/ / _ `/ // / _ \ |/|/ / _ \/ / _ \/ _ `/ _  / -_) __/'
+echo '/_/  /_/\_,_/_/ /_/\_,_/____/\___/__,__/_//_/_/\___/\_,_/\_,_/\__/_/'
+echo '   _          __       ____'
+echo '  (_)__  ___ / /____ _/ / /__ ____'
+echo ' / / _ \(_-</ __/ _ `/ / / -_) __/'
+echo '/_/_//_/___/\__/\_,_/_/_/\__/_/'
+echo " "
+
+echo "Installing dependencies..."
+sudo apt install wget unzip -y &>/dev/null
+sudo pacman -S wget unzip --noconfirm &>/dev/null
+echo "All dependencies are now installed"
+
+echo "Downloading MariaDownloader"
+wget https://raw.githubusercontent.com/LenninZiony/MariaDownloader/main/bin/mariadownloader -O /tmp/mariadownloader
+sudo chown root:root /tmp/mariadownloader
+sudo chmod 755 /tmp/mariadownloader
+sudo mv /tmp/mariadownloader /bin/mariadownloader
+echo "MariaDownloader was installed"
