1f05346eaaa64433a0320810ca9e83228383457e
Lennin <80495420+LenninZiony@users.noreply.github.com>
2022-01-27 01:11:47 -0300

    :)

--- mariadownloader.py
+++ mariadownloader.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
 
-import wget,zipfile
+import readline,colorama,wget,zipfile
+from colorama import Fore
+colorama.init(autoreset=True)
 
-print("""
-   __  ___         _      ___                  __             __
+print("""   __  ___         _      ___                  __             __
   /  |/  /__ _____(_)__ _/ _ \___ _    _____  / /__  ___ ____/ /__ ____
  / /|_/ / _ `/ __/ / _ `/ // / _ \ |/|/ / _ \/ / _ \/ _ `/ _  / -_) __/
-/_/  /_/\_,_/_/ /_/\_,_/____/\___/__,__/_//_/_/\___/\_,_/\_,_/\__/_/
-                                    by @LenninZiony and @playermushroom
-""")
+/_/  /_/\_,_/_/ /_/\_,_/____/\___/__,__/_//_/_/\___/\_,_/\_,_/\__/_/""")
+print(Fore.CYAN + "                                    by @LenninZiony and @playermushroom")
 
-mdurl = input("File URL: ")
-mdfile = input("File name: ")
-mdzip = input("Unzip the file? [y/N] ")
+try:
+    mdurl = input("File URL: ")
+    mdfile = input("File name: ")
+    mdzip = input("Unzip the file? [y/N] ")
 
-if (mdzip == "y" or mdzip == "Y" or mdzip == "yes" or mdzip == "YES"):
+    def mddownload():
+        print("Mariadownloading...")
+        wget.download(mdurl, mdfile)
+        print()
+        print("Mariadownloaded!")
 
-    mddir = input("Directory to extract: ")
-    print("Mariadownloading...")
-    wget.download(mdurl, mdfile)
-    print()
-    print("Mariadownloaded!")
+    def mdunzip():
+        print("Unzipping...")
+        with zipfile.ZipFile(mdfile, 'r') as zip_ref:
+            zip_ref.extractall(mddir)
+        print("Unzipped")
+        print("Saved under " + mddir)
 
-    print("Unzipping...")
-    with zipfile.ZipFile(mdfile, 'r') as zip_ref:
-        zip_ref.extractall(mddir)
-    print("Unzipped")
+    if (mdzip == "y" or mdzip == "Y" or mdzip == "yes" or mdzip == "YES"):
+        mddir = input("Directory to extract: ")
+        mddownload()
+        mdunzip()
 
-    print("Saved under " + mddir + "/")
+    else:
+        mddownload()
 
-else:
-
-    print("Mariadownloading...")
-    wget.download(mdurl, mdfile)
-    print()
-    print("Mariadownloaded!")
-    print("Saved as " + mdfile)
+except KeyboardInterrupt:
+    print("")
